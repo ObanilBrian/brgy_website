b4cc5caa1ebce508e73d39c352ea79693fd9cfc3
Brian Obanil <obanil.brian@outlook.com>
2018-02-23 22:09:21 +0800

    added migration

---
+++ priv/repo/migrations/20180223132135_add_users.exs
@@ -0,0 +1,11 @@
+defmodule BrgyWebsite.Repo.Migrations.AddUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      
+      timestamps()
+    end
+  end
+end
