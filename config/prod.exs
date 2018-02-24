65be329b1301062d977e7b8b221c9c5634a44566
Brian Obanil <obanil.brian@outlook.com>
2018-02-25 00:00:30 +0800

    changes

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :brgy_website, BrgyWebsiteWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "brgy-website.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache-manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
