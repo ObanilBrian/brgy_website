1b0cbc8f2549e439dc651756ab075afaf15f0649
Brian Obanil <obanil.brian@outlook.com>
2018-02-28 05:46:54 +0800

    added routes

--- lib/brgy_website_web/controllers/page_controller.ex
+++ lib/brgy_website_web/controllers/page_controller.ex
@@ -1,5 +1,14 @@
 defmodule BrgyWebsiteWeb.PageController do
   use BrgyWebsiteWeb, :controller
+  import Ecto.{Query, Changeset}, warn: false
+
+  alias BrgyWebsite.{
+    Repo,
+    Schemas.User,
+    Schemas.Blotter,
+    Schemas.News,
+    Schemas.Official,
+  }
 
   def index(conn, _params) do
     render conn, "index.html"
@@ -16,4 +25,40 @@ defmodule BrgyWebsiteWeb.PageController do
   def login(conn, _params) do
     render conn, "login.html"
   end
+
+  def load_all_blotters(conn, _params) do
+    blotters = 
+      Blotter
+      |> Repo.all
+
+    conn
+    |> render(
+      "blotter.html",
+      blotters: blotters) 
+  end
+
+  def edit_blotters(conn, %{"id" => id}) do
+    blotter =
+      Blotter
+      |> where([b], b.id == ^id)
+      |> Repo.one
+    
+    conn
+    |> render(
+      "edit_blotter.html",
+      blotter: blotter) 
+  end
+
+  def add_blotters(conn, _params) do
+    %Blotter{}
+    |> Blotter.changeset(_params)
+    |> Repo.insert!
+    
+    conn
+    |> render(
+      "edit_blotter.html",
+      blotter: blotter) 
+  end
+
+  
 end
--- lib/brgy_website_web/router.ex
+++ lib/brgy_website_web/router.ex
@@ -21,6 +21,31 @@ defmodule BrgyWebsiteWeb.Router do
     get "/report", PageController, :report
 
     get "/login", PageController, :login
+    
+    get "/blotter", PageController, :load_all_blotters
+    get "/blotter/:id", PageController, :edit_blotter
+    post "/blotter", PageController, :add_blotter
+    put "/blotter/:id", PageController, :update_blotter
+    delete "/blotter/:id", PageController, :delete_blotter
+
+    get "/news", PageController, :load_all_news
+    get "/news/:id", PageController, :edit_news
+    post "/news", PageController, :add_news
+    put "/news/:id", PageController, :update_news
+    delete "/news/:id", PageController, :delete_news
+
+    get "/official", PageController, :load_all_official
+    get "/official/:id", PageController, :edit_official
+    post "/official", PageController, :add_official
+    put "/official/:id", PageController, :update_official
+    delete "/official/:id", PageController, :delete_official
+
+    get "/user", PageController, :load_all_user
+    get "/user/:id", PageController, :edit_user
+    post "/user", PageController, :add_user
+    put "/user/:id", PageController, :update_user
+    delete "/user/:id", PageController, :delete_user
+
   end
 
   # Other scopes may use custom stacks.
